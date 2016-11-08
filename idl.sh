58f716d248bbac68bfd4382bfde88349a1322e2e
Stig Bakken <stig@zedge.net>
2016-11-08 10:58:29 +0100

    idl.sh: more userfriendly error if running non-virtualbox VM

--- idl.sh
+++ idl.sh
@@ -6,22 +6,29 @@ fi
 repo_root="$(git rev-parse --show-toplevel)"
 
 if [ -n "$DOCKER_MACHINE_NAME" ]; then
-    # This contortion is for preserving your ssh-agent into a container running in a VM.
-    # Only tested with local VirtualBox-based Docker Machines.
-    ssh_key=$HOME/.docker/machine/machines/${DOCKER_MACHINE_NAME}/id_rsa
-    ssh_host=$(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r .Driver.IPAddress)
-    ssh_port=$(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r .Driver.SSHPort)
-    ssh_user=$(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r .Driver.SSHUser)
-    ssh -A -o NoHostAuthenticationForLocalhost=yes -i ${ssh_key} -p ${ssh_port} -l ${ssh_user} localhost \
-        "docker run -v \$SSH_AUTH_SOCK:\$SSH_AUTH_SOCK -e SSH_AUTH_SOCK=\$SSH_AUTH_SOCK -u 1000 \
+    dm_driver="$(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r .DriverName)"
+    # This contortion is needed to preserve your ssh-agent into a container running in a VM.
+    case ${dm_driver} in
+	virtualbox)
+	    ssh_key=$HOME/.docker/machine/machines/${DOCKER_MACHINE_NAME}/id_rsa
+	    ssh_host=$(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r .Driver.IPAddress)
+	    ssh_port=$(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r .Driver.SSHPort)
+	    ssh_user=$(docker-machine inspect ${DOCKER_MACHINE_NAME} | jq -r .Driver.SSHUser)
+	    ssh -A -o NoHostAuthenticationForLocalhost=yes -i ${ssh_key} -p ${ssh_port} -l ${ssh_user} localhost \
+		"docker run -e SSH_AUTH_SOCK=\$SSH_AUTH_SOCK -v \$SSH_AUTH_SOCK:\$SSH_AUTH_SOCK -u 1000 \
                     -e HOME=$HOME -v $HOME/.idl:$HOME/.idl -v $repo_root:$repo_root -w $(pwd) \
                     zedge/idl $@"
+	    ;;
+	*)
+	    echo "$(basename $0): unsupported docker-machine driver: ${dm_driver}" >&2
+	    exit 1
+	    ;;
+    esac
 else
     uid=$(id)
     uid=${uid#uid=}
     uid=${uid%%(*}
-    docker run -v ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} -u ${uid} \
-                -e HOME=$HOME -v $HOME/.idl:$HOME/.idl -v ${repo_root}:${repo_root} -w $(pwd) \
-                zedge/idl $@
-
+    docker run -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK -u ${uid} \
+               -e HOME=$HOME -v $HOME/.idl:$HOME/.idl -v $repo_root:$repo_root -w $(pwd) \
+                    zedge/idl $@
 fi
